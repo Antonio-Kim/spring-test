65ac6d8c134bd63b964ef1b45365ede878fb0fd8
Antonio Kim <antoniok@sfu.ca>
2024-02-22 13:55:43 -0800

    Using Spring TestContext for testing

---
+++ src/test/java/SpringAppTest.java
@@ -0,0 +1,29 @@
+import com.antoniok.Passenger;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:application-context.xml")
+public class SpringAppTest {
+
+    @Autowired
+    private Passenger passenger;
+    private Passenger expectedPassenger;
+
+    @Before
+    public void setUp() {
+        expectedPassenger = PassengerUtil.getExpectedPassenger();
+    }
+
+    @Test
+    public void testInitPassenger() {
+        assertEquals(expectedPassenger, passenger);
+        System.out.println(passenger);
+    }
+}
